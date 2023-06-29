cf431f8f93d951115960bfbb0b47dca7fccb7e01
Pascal ter Horst <pascal.terhorst@petsdeli.de>
2023-06-29 17:03:48 +0200

    layout update modal

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
+function formatData(data, container) {
+  for (let key in data) {
+    let section = document.createElement('div');
+    section.className = 'section';
+    section.innerHTML = `<h2>${key}:</h2>`;
+    if (typeof data[key] === 'object') {
+      if (Array.isArray(data[key])) {
+        section.innerHTML += data[key].map(item => `<p>${item}</p>`).join('');
+      } else {
+        formatData(data[key], section);
+      }
+    } else {
+      section.innerHTML += `<p>${data[key]}</p>`;
+    }
+    container.appendChild(section);
+  }
+}
+
 chrome.storage.local.get(['nt_profile'], function(result) {
-    document.getElementById('data').textContent = JSON.stringify(result.nt_profile, null, 2);
-  });
+  let container = document.getElementById('data');
+  formatData(result.nt_profile, container);
+});
