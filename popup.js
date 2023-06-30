cbabe3a7ded94e86a834cd6bf9f05b986ea40b82
Pascal ter Horst <pascal.terhorst@petsdeli.de>
2023-06-30 12:00:48 +0200

    add hyper links to audience

--- popup.js
+++ popup.js
@@ -8,32 +8,30 @@ document.getElementById('refreshBtn').addEventListener('click', function() {
 chrome.storage.local.get(['nt_profile'], function(result) {
   let dataToDisplay = JSON.stringify(result.nt_profile, null, 2);
 
-// Handle checkbox changes
-document.getElementById('showAll').addEventListener('change', function(e) {
-  if (e.target.checked) {
+  // Handle checkbox changes
+  document.getElementById('showAll').addEventListener('change', function(e) {
+    if (e.target.checked) {
       document.getElementById('showAudiences').checked = false;
       document.getElementById('showTraits').checked = false;
       displayData(result.nt_profile);
-  }
-});
+    }
+  });
 
-document.getElementById('showAudiences').addEventListener('change', function(e) {
-  if (e.target.checked) {
+  document.getElementById('showAudiences').addEventListener('change', function(e) {
+    if (e.target.checked) {
       document.getElementById('showAll').checked = false;
       document.getElementById('showTraits').checked = false;
       displayData({audiences: result.nt_profile.audiences});
-  }
-});
+    }
+  });
 
-document.getElementById('showTraits').addEventListener('change', function(e) {
-  if (e.target.checked) {
+  document.getElementById('showTraits').addEventListener('change', function(e) {
+    if (e.target.checked) {
       document.getElementById('showAll').checked = false;
       document.getElementById('showAudiences').checked = false;
       displayData({traits: result.nt_profile.traits});
-  }
-});
-
-
+    }
+  });
 
   // Default to show all
   displayData(result.nt_profile);
@@ -56,9 +54,21 @@ function renderJson(jsonData) {
 }
 
 function createKeyValuePair(data, key) {
-  const p = document.createElement('p');
-  p.textContent = `${key} : ${data[key]}`;
-  return p;
+  const keyValueDiv = document.createElement('div');
+  const pKey = document.createElement('p');
+  pKey.textContent = `${key} :`;
+
+  // If the data is an object, create a new section for it
+  if (typeof data[key] === 'object' && data[key] !== null && !Array.isArray(data[key])) {
+    const sectionDiv = createSection(data[key]);
+    keyValueDiv.appendChild(pKey);
+    keyValueDiv.appendChild(sectionDiv);
+  } else {
+    pKey.textContent += ` ${data[key]}`;
+    keyValueDiv.appendChild(pKey);
+  }
+
+  return keyValueDiv;
 }
 
 
@@ -74,7 +84,18 @@ function createSection(data, key) {
     const ul = document.createElement('ul');
     data[key].forEach(item => {
       const li = document.createElement('li');
-      li.textContent = item;
+      
+      // Check if key is 'audiences' to add hyperlink
+      if(key === 'audiences') {
+        const a = document.createElement('a');
+        a.href = `https://app.contentful.com/spaces/s8qothrtqrf9/entries/${item}`;
+        a.textContent = item;
+        a.target = "_blank";
+        li.appendChild(a);
+      } else {
+        li.textContent = item;
+      }
+      
       ul.appendChild(li);
     });
     sectionDiv.appendChild(ul);
